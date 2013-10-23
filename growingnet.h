362d74ad51d3c2d78134fac886d594d1b6c092af
Hubert T <ht335965@students.mimuw.edu.pl>
2013-10-23 20:55:33 +0200

    Deklaracje funkcji interfejsu z growingnet.h.

---
+++ growingnet.h
@@ -0,0 +1,3 @@
+//Poniższa stała jest identyfikatorem Globalnej Sieci Rosnącej.
+//Do sieci rosnącej można dodawać węzły i krawędzie, ale nie można ich usuwać.
+extern const unsigned long growingnet;
