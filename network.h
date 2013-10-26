cdc50837282b12b3b4105c0b304542e8bd446124
Jacek Koziński <ubuntu@ubuntu-VirtualBox.(none)>
2013-10-26 15:46:35 +0200

    Translated comments in network.h

--- network.h
+++ network.h
@@ -7,51 +7,52 @@
 extern "C" {
 #endif
 
-//Tworzy nową, pustą, sieć i zwraca jej identyfikator.
-//Sieć pusta, to sieć z pustym zbiorem węzłów.
-//Parametr growing mówi o tym, czy nowa sieć ma być rosnąca (growing != 0) czy nie (growing == 0).
+//Creates a new empty network and returns its id.
+//An empty network is a network with empty set of nodes.
+//Argument growing indicates whether the network is to be growing (growing != 0) or not (growing == 0).
 unsigned long network_new(int growing);
 
-//Jeżeli istnieje sieć o identyfikatorze id, to usuwa sieć, a w przeciwnym przypadku nic nie robi.
+//If there exists a network with given id then it is deleted, otherwise nothing is done.
 void network_delete(unsigned long id);
 
-//Jeżeli istnieje sieć o identyfikatorze id, zwraca liczbę jej węzłów, a w przeciwnym przypadku zwraca 0.
+//If there exists a network with given id then returns the number of nodes it contains, otherwise returns 0.
 size_t network_nodes_number(unsigned long id);
 
-//Jeżeli istnieje sieć o identyfikatorze id, zwraca liczbę jej krawędzi, a w przeciwnym przypadku zwraca 0.
+//If there exists a network with given id then returns the number of links it contains, otherwise returns 0.
 size_t network_links_number(unsigned long id);
 
-//Jeżeli istnieje sieć o identyfikatorze id, label != NULL
-//i sieć ta nie zawiera jeszcze węzła o etykiecie label,
-//to dodaje węzeł o etykiecie label do sieci, a w przeciwnym przypadku nic nie robi.
+//If there exists a network with given id, label != NULL
+//and the network does not contain node named label then
+//adds node named label to the network, otherwise nothing is done.
 void network_add_node(unsigned long id, const char* label);
 
-//Jeżeli istnieje sieć o identyfikatorze id, slabel != NULL oraz tlabel != NULL,
-//i sieć ta nie zawiera jeszcze krawędzi (slabel, tlabel), to dodaje krawędź
-//(slabel, tlabel) do sieci, a w przeciwnym przypadu nic nie robi.
-//Jeżeli w sieci nie istnieje węzeł o etykiecie któregoś z końców krawędzi,
-//to jest on również dodawany.
+//If there exists a network with given id, slabel != NULL and tlabel != NULL,
+//and the network does not contain link (slabel, tlabel) then adds link
+//(slabel, tlabel) to the network, otherwise nothing is done.
+//If the network does not contain either or both of the nodes marking the ends of the link
+//then required node(s) is(are) added to the network as well.
 void network_add_link(unsigned long id, const char* slabel, const char* tlabel);
 
-//Jeżeli istnieje sieć o identyfikatorze, a w niej węzeł o etykiecie label
-//oraz sieć nie jest rosnąca, to usuwa węzeł z sieci wraz
-//ze wszystkimi krawędziami wchodzącymi i wychodzącymi, a w przeciwnym przypadku nic nie robi.
+//If there exists a network with given id and it contains node named label
+//and the network is not growing then the node is deleted from the network
+//along with its every incoming and outgoing link, otherwise nothing is done.
 void network_remove_node(unsigned long id, const char* label);
 
-//Jeżeli istnieje sieć o identyfikatorze id, a w niej krawędź (slabel, tlabel),
-//oraz sieć nie jest rosnąca, to usuwa krawędź z sieci, a w przeciwnym przypadku nic nie robi.
+//If there exists a network with given id and it contains link (slabel, tlabel)
+//and the network is not growing then the link is deleted from the network,
+//otherwise nothing is done.
 void network_remove_link(unsigned long id, const char* slabel, const char* tlabel);
 
-//Jeżeli istnieje sieć o identyfikatorze id i sieć nie jest rosnąca,
-//usuwa wszystkie jej węzły i krawędzie, a w przeciwnym przypadku nic nie robi.
+//If there exists a network with given id and it is not growing
+//then each and every of its nodes and links are deleted, otherwise nothing is done.
 void network_clear(unsigned long id);
 
-//Jeżeli istnieje sieć o identyfikatorze id, a w niej węzeł o etykiecie label,
-//to zwraca ilość krawędzi wychodzących z tego węzła, a w przeciwnym przypadku zwraca 0.
+//If there exists a network with given id and it contains node named label
+//then it returns the number of the node's outgoing links, otherwise returns 0.
 size_t network_out_degree(unsigned long id, const char* label);
 
-//Jeżeli istnieje sieć o identyfikatorze id, a w niej węzeł o etykiecie label,
-//to zwraca ilość krawędzi wchodzących do tego węzła, a w przeciwnym przypadku zwraca 0.
+//If there exists a network with given id and it contains node named label
+//then it returns the number of the node's incoming links, otherwise returns 0.
 size_t network_in_degree(unsigned long id, const char* label);
 
 #ifdef __cplusplus
