81365829c72be9b2b876da64b1a70db6aaf5e142
Hubert T <ht335965@students.mimuw.edu.pl>
2013-10-29 18:42:59 +0100

    Dodaję extern "C" do growingnet.h.

--- growingnet.h
+++ growingnet.h
@@ -1,8 +1,16 @@
 #ifndef _GROWING_NET_H
 #define _GROWING_NET_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif	//__cplusplus
+
 //The constant below is the identifier of Global Growing Network
 //Nodes and links can be added to a growing network, but they cannot be deleted.
 extern const unsigned long growingnet;
 
-#endif
+#ifdef __cplusplus
+}
+#endif  //__cplusplus
+
+#endif  //_GROWING_NET_H
