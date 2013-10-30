f794cb7e680161284c06b47e8d912e5e1cd07a54
Hubert T <ht335965@students.mimuw.edu.pl>
2013-10-30 22:44:22 +0100

    net_test poprawki

--- net_test.c
+++ net_test.c
@@ -5,20 +5,21 @@
 
 int main()
 {
-	printf("hello\n");
-	
-	unsigned long id = network_new(0);
-	network_nodes_number(id);
-	network_add_node(id, "test1");	
-	network_add_node(id, "test2");
-	network_new(0);
-	network_new(0);
-	network_nodes_number(id);
-	network_clear(id);
-	network_nodes_number(id);
-	network_out_degree(id, "test1");
-	network_add_link(id, "test1", "test2");
-	network_out_degree(id, "test1");
-	
-	return 0;
+    printf("Network Testing Application\n");
+    
+    unsigned long id = network_new(0);
+    network_nodes_number(id);
+    network_nodes_number(-1);
+    network_add_node(id, "test1");  
+    network_add_node(id, "test2");
+    network_new(0);
+    network_new(0);
+    network_nodes_number(id);
+    network_clear(id);
+    network_nodes_number(id);
+    network_out_degree(id, "test1");
+    network_add_link(id, "test1", "test2");
+    network_out_degree(id, "test1");
+    
+    return 0;
 }
