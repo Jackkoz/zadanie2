c8b83402144fc9533f1be021098aeac5abd11ab4
Hubert T <ht335965@students.mimuw.edu.pl>
2013-10-30 19:52:08 +0100

    Komentarze w growingnet.h

--- growingnet.h
+++ growingnet.h
@@ -1,16 +1,24 @@
 #ifndef _GROWING_NET_H
 #define _GROWING_NET_H
 
+
+// We want to disable name mangling in order for the module
+// to be usable with programs written in C.
 #ifdef __cplusplus
 extern "C" {
-#endif	//__cplusplus
+#endif	// #ifdef __cplusplus
 
-// The constant below is the identifier of Global Growing Network.
-// Nodes and links can be added to a growing network, but they cannot be deleted.
+
+/*
+ * The constant below is the identifier of Global Growing Network.
+ * Nodes and links can be added to a growing network, but they cannot be deleted.
+ * 
+ */
 extern const unsigned long growingnet;
+
 
 #ifdef __cplusplus
 }
-#endif  //__cplusplus
+#endif  // #ifdef __cplusplus
 
-#endif  //_GROWING_NET_H
+#endif  // #ifndef _GROWING_NET_H
