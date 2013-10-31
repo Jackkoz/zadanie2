18fab02d8596ed65526650fee20a7b78d534727a
Hubert T <ht335965@students.mimuw.edu.pl>
2013-10-31 02:20:50 +0100

    Zmieniam komunikat null label.

--- network.cc
+++ network.cc
@@ -47,7 +47,7 @@ typedef map<unsigned long, NET> NET_CONTAINER;
 static const string CE_NETWORK_NOT_FOUND = "Network not found.";
 static const string CE_NODE_NOT_FOUND = "Node not found.";
 static const string CE_LINK_NOT_FOUND = "Link not found.";
-static const string CE_LABEL_IS_NULL = "Label is NULL.";
+static const string CE_LABEL_IS_NULL = "Given a NULL label.";
 static const string CE_NETWORK_IS_GROWING = "Network is growing.";
 static const string CE_FATAL = "Cannot continue. Returning neutral value or void.";
 /**********************************************************************/
